93f17c93589d8d0309689247bb87eb7d974b09ca
Ivan <176309295+lostmusician@users.noreply.github.com>
2026-09-11 23:18:35 +0800

    feat(ui): polish light and economy HUD

--- ui/sidebar.gd
+++ ui/sidebar.gd
@@ -5,6 +5,9 @@ signal fish_purchase_requested
 const CATALOG := preload("res://resources/species_catalog.tres")
 const SELECT_FISH_BUTTON_SCENE := preload("res://ui/select_fish_button.tscn")
 
+@onready var money_value: Label = $TopSpacer/EconomyPanel/EconomyContent/MoneyValue
+@onready var beauty_value: Label = $TopSpacer/EconomyPanel/EconomyContent/BeautyValue
+
 func _ready() -> void:
 	#load all available fish species
 	#for species in CATALOG.fish_species:
@@ -19,7 +22,8 @@ func _ready() -> void:
 	$StatusPanel/ProgressBarO2.value = Aquarium.O2Level
 	$StatusPanel/ProgressBarCO2.value = Aquarium.CO2Level
 	$StatusPanel/ProgressBarFood.value = Aquarium.FoodLevel
-	$LabelMoney.text = "Money: $" + str(int(Aquarium.Money))
+	money_value.text = str(int(Aquarium.Money))
+	beauty_value.text = str(int(Aquarium.Beauty))
 
 func _on_purchase_requested(species: FishSpecies) -> void:
 	fish_purchase_requested.emit(species)
@@ -28,8 +32,11 @@ func _on_stats_changed() -> void:
 	$StatusPanel/ProgressBarO2.value = Aquarium.O2Level
 	$StatusPanel/ProgressBarCO2.value = Aquarium.CO2Level
 	$StatusPanel/ProgressBarFood.value = Aquarium.FoodLevel
-	$LabelBeauty.text = "Beauty: " + str(Aquarium.Beauty)
-	$LabelMoney.text = "Money: $" + str(int(Aquarium.Money))
+	beauty_value.text = str(int(Aquarium.Beauty))
+	money_value.text = str(int(Aquarium.Money))
 
 func _on_cash_changed(new_cash: float) -> void:
-	$LabelMoney.text = "Money: $%d" % int(new_cash)
+	money_value.text = str(int(new_cash))
+	money_value.self_modulate = Color(1.25, 1.12, 0.75, 1.0)
+	var tween := create_tween()
+	tween.tween_property(money_value, "self_modulate", Color.WHITE, 0.2)
